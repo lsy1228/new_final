8711ee8fd91e6dcc55c4b646da7aab617506637d
LeeSoYoung <258735@naver.com>
2024-11-30 18:33:51 +0900

    커밋

---
+++ src/main/java/com/khh/new_final/NewFinalApplication.java
@@ -0,0 +1,13 @@
+package com.khh.new_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khh/new_final/NewFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khh.new_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
